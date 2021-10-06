4b573cd244e5499ad9a8e2ad43e6d5fcbfc9edac
Prophet-99 <alexanderavilab@gmail.com>
2021-10-06 17:31:46 -0500

    add new resources

--- js/flipbook-desktop-template.js
+++ js/flipbook-desktop-template.js
@@ -3,19 +3,19 @@ const flipbookDesktopTemplate = () => (
     <div class="wedding__count-section-clock">
       <p class="wedding__count-section-item">
         <span class="wedding__count-section-subitem">02</span>
-        <span class="wedding__count-section-subitem--mod">Months</span>
+        <span class="wedding__count-section-subitem--mod">Meses</span>
       </p>
       <p class="wedding__count-section-item">
         <span class="wedding__count-section-subitem">15</span>
-        <span class="wedding__count-section-subitem--mod">Days</span>
+        <span class="wedding__count-section-subitem--mod">Días</span>
       </p>
       <p class="wedding__count-section-item">
         <span class="wedding__count-section-subitem">02</span>
-        <span class="wedding__count-section-subitem--mod">Hours</span>
+        <span class="wedding__count-section-subitem--mod">Horas</span>
       </p>
       <p class="wedding__count-section-item">
         <span class="wedding__count-section-subitem">34</span>
-        <span class="wedding__count-section-subitem--mod">Minutes</span>
+        <span class="wedding__count-section-subitem--mod">Minutos</span>
       </p>
     </div>
   </section>
@@ -23,23 +23,10 @@ const flipbookDesktopTemplate = () => (
   <section class="wedding__flipbook-section js-flipbook-content">
     <div class="flipbook">
       <!-- <div ignore="1"> Something else </div> -->
-      <div class="wedding__flipbook-content">
-        <article class="wedding__flipbook-paragraph">
-          <h3 style="text-align: center;">
-            Bienvenidos a esta tarjeta
-          </h3>
-          <p style="text-align: center;">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint earum illum autem itaque, blanditiis beatae iure quo nisi amet, atque rerum placeat et culpa. Minima earum error sint velit!
-          </p>
-          <button onclick="nextPage()">
-            Next
-          </button>
-        </article>
-      </div>
-      <div class="wedding_principal-section"></div>
-      <div class="wedding_reception-section"></div>
-      <div class="wedding_godparents-section"></div>
-      <div class="wedding_gifts-section"></div>
+      <div class="wedding__content-background wedding__principal-section"></div>
+      <div class="wedding__content-background wedding__reception-section"></div>
+      <div class="wedding__content-background wedding__godparents-section"></div>
+      <div class="wedding__content-background wedding__gifts-section"></div>
     </div>
   </section>`
 );
--- js/flipbook-mobile-template.js
+++ js/flipbook-mobile-template.js
@@ -3,23 +3,12 @@ const flipbookMobileTemplate = () => (
   <section class="wedding__flipbook-section js-flipbook-content">
     <div class="flipbook">
       <!-- <div ignore="1"> Something else </div> -->
-      <div class="wedding__flipbook-content">
-        <article class="wedding__flipbook-paragraph">
-          <h3 style="text-align: center;">
-            Bienvenidos a esta tarjeta
-          </h3>
-          <p style="text-align: center;">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint earum illum autem itaque, blanditiis beatae iure quo nisi amet, atque rerum placeat et culpa. Minima earum error sint velit!
-          </p>
-          <button onclick="nextPage()">
-            Next
-          </button>
-        </article>
-      </div>
-      <div class="wedding_principal-section"></div>
-      <div class="wedding_reception-section"></div>
-      <div class="wedding_godparents-section"></div>
-      <div class="wedding_gifts-section"></div>
+      <div class="wedding__content-background wedding__principal-section is-mobile"></div>
+      <div class="wedding__content-background wedding__principal-section is-mobile"></div>
+      <div class="wedding__content-background wedding__reception-section is-mobile"></div>
+      <div class="wedding__content-background wedding__reception-section is-mobile"></div>
+      <div class="wedding__content-background wedding__godparents-section is-mobile"></div>
+      <div class="wedding__content-background wedding__gifts-section is-mobile"></div>
     </div>
   </section>`
 );
