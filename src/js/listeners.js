4c40c8b63ec9971a32de853279126807a53a1d2a
Prophet-99 <alexanderavilab@gmail.com>
2021-11-17 13:29:53 -0500

    Added counter and first version of loader

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -5,9 +5,40 @@ const mobileListener = () => {
   $('.flipbook').turn('size', '200vw', flipbookSize.height);
 };
 
-const desktopListener = () => 1;
+const handleSetDigitFormat = (digit) => ((String(digit).length === 1) ? `0${digit}` : digit);
+
+const counterListener = (stringDate = '2040-01-13 15:30:00') => {
+  const dateTarget = new Date(stringDate);
+  // DOM REFERENCES
+  const DOMSeconds = document.querySelector('.js-seconds');
+  const DOMMinutes = document.querySelector('.js-minutes');
+  const DOMHours = document.querySelector('.js-hours');
+  const DOMDays = document.querySelector('.js-days');
+  // TIME REFERENCES
+  const milisecondsInASecond = 1000;
+  const milisecondsInAMinute = milisecondsInASecond * 60;
+  const milisecondsInAHour = milisecondsInAMinute * 60;
+  const milisecondsInADay = milisecondsInAHour * 24;
+  const processReference = () => {
+    const now = new Date();
+    const remainingTime = dateTarget - now;
+    DOMDays.textContent = handleSetDigitFormat(
+      Math.floor(remainingTime / milisecondsInADay),
+    );
+    DOMHours.textContent = handleSetDigitFormat(
+      Math.floor((remainingTime % milisecondsInADay) / milisecondsInAHour),
+    );
+    DOMMinutes.textContent = handleSetDigitFormat(
+      Math.floor((remainingTime % milisecondsInAHour) / milisecondsInAMinute),
+    );
+    DOMSeconds.textContent = handleSetDigitFormat(
+      Math.floor((remainingTime % milisecondsInAMinute) / milisecondsInASecond),
+    );
+  };
+  setInterval(processReference, milisecondsInASecond);
+};
 
 export {
   mobileListener,
-  desktopListener,
+  counterListener,
 };
--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import './scss/styles.scss';
-import HTMLDesktopTemplate from './templates/flipbook-desktop.template.html';
+import HTMLDesktopTemplate from './templates/swiper-desktop.template.html';
 import HTMLMobileTemplate from './templates/flipbook-mobile.template.html';
+import HTMLLoaderTemplate from './templates/loader.template.html';
 
-import { mobileListener } from './js/listeners';
+import { counterListener, mobileListener } from './js/listeners';
 import flipbookInitializer from './js/flipbook-initializer';
 import swiperInitializer from './js/swiper-initializer';
 
 const smallBp = matchMedia('(min-width: 576px)');
 const setContentVariable = (mql) => {
   if (mql.matches) { // GREATHER THAN 576px
-    document.querySelector('.js-main-content').innerHTML = HTMLDesktopTemplate;
+    document.querySelector('.js-main-content').innerHTML = HTMLLoaderTemplate;
     swiperInitializer();
+    counterListener('2022-02-05 12:30:00');
   } else { // LOWER THAN 576px
     document.querySelector('.js-main-content').innerHTML = HTMLMobileTemplate;
     setTimeout(() => {
       flipbookInitializer();
-      // RESIZE FIPBOOK
-      mobileListener();
+      mobileListener(); // TO RESIZE FIPBOOK
+      counterListener('2022-02-05 12:30:00');
     },
     0);
   }
