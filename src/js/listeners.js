3320e9121845509f142907f4064e2d208e82b281
Prophet-99 <alexanderavilab@gmail.com>
2021-12-03 14:08:21 -0500

    Added fetch API to QR code with loader

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,3 +1,26 @@
+const capturingQueryParam = async () => {
+  const queryParam = window.location.search;
+  const urlParam = new URLSearchParams(queryParam);
+  const code = urlParam.get('code');
+  if (code) {
+    try {
+      let parsedData;
+      if (!sessionStorage.getItem('WEDDING_QR_CODE')) {
+        const rawData = await fetch(`http://134.122.124.135:2000/api/v1/wedding/invitation/${code}`);
+        parsedData = await rawData.json();
+        if (parsedData.length === 0) throw new Error();
+        else sessionStorage.setItem('WEDDING_QR_CODE', JSON.stringify(parsedData));
+      } else {
+        const sessionData = sessionStorage.getItem('WEDDING_QR_CODE');
+        parsedData = JSON.parse(sessionData);
+      }
+      return new Promise((resolve) => setTimeout(resolve, 1000, parsedData));
+    } catch (_) {
+      return new Promise((resolve) => setTimeout(resolve, 2000, false));
+    }
+  } else return new Promise((resolve) => setTimeout(resolve, 2000, false));
+};
+
 const mobileListener = () => {
   const flipbookContentRef = document.querySelector('.js-flipbook-content');
   const flipbookSize = flipbookContentRef.getBoundingClientRect();
@@ -44,8 +67,20 @@ const hideLoaderListener = (callback) => {
   setTimeout(callback, 300);
 };
 
+const renderQRCode = (qrCodeRef) => {
+  if (qrCodeRef) {
+    document.querySelector('.js-qr-code').setAttribute('src', qrCodeRef[0].url);
+    setTimeout(() => {
+      document.querySelector('.js-qr-loader').classList.add('is-hidden');
+      document.querySelector('.js-qr-code').classList.remove('is-hidden');
+    }, 1000);
+  } else document.querySelector('.js-qr-loader').classList.add('is-hidden');
+};
+
 export {
+  capturingQueryParam,
   mobileListener,
   counterListener,
   hideLoaderListener,
+  renderQRCode,
 };
--- src/main.js
+++ src/main.js
@@ -5,32 +5,42 @@ import HTMLDesktopTemplate from './templates/swiper-desktop.template.html';
 import HTMLMobileTemplate from './templates/flipbook-mobile.template.html';
 import HTMLLoaderTemplate from './templates/loader.template.html';
 
-import { counterListener, mobileListener, hideLoaderListener } from './js/listeners';
+import {
+  counterListener, mobileListener, hideLoaderListener,
+  capturingQueryParam, renderQRCode,
+} from './js/listeners';
 import flipbookInitializer from './js/flipbook-initializer';
 import swiperInitializer from './js/swiper-initializer';
 
 const smallBp = matchMedia('(min-width: 576px)');
-const setContentVariable = (mql) => {
+const setContentVariable = async (mql) => {
   document.querySelector('.js-main-content').innerHTML = HTMLLoaderTemplate;
   if (mql.matches) { // GREATHER THAN 576px
-    // TODO: TO SIMULATE LOADING - IN THE FUTURE FUNCTION WITH CALLBACK
-    setTimeout(() => {
-      hideLoaderListener(() => {
-        document.querySelector('.js-main-content').innerHTML = HTMLDesktopTemplate;
-        swiperInitializer();
-        counterListener('2022-02-05 12:30:00');
-      });
-    }, 2000);
+    const qrCode = await capturingQueryParam();
+    hideLoaderListener(() => {
+      document.querySelector('.js-main-content').innerHTML = HTMLDesktopTemplate;
+      swiperInitializer();
+      counterListener('2022-02-05 12:30:00');
+      // INSERTING QR CODE IN THE DOM
+      renderQRCode(qrCode);
+      if (qrCode) {
+        document.querySelector('.js-qr-code').setAttribute('src', qrCode[0].url);
+        setTimeout(() => {
+          document.querySelector('.js-qr-loader').classList.add('is-hidden');
+          document.querySelector('.js-qr-code').classList.remove('is-hidden');
+        }, 1000);
+      } else document.querySelector('.js-qr-loader').classList.add('is-hidden');
+    });
   } else { // LOWER THAN 576px
-    // TODO: TO SIMULATE LOADING - IN THE FUTURE FUNCTION WITH CALLBACK
-    setTimeout(() => {
-      hideLoaderListener(() => {
-        document.querySelector('.js-main-content').innerHTML = HTMLMobileTemplate;
-        flipbookInitializer();
-        mobileListener(); // TO RESIZE FIPBOOK
-        counterListener('2022-02-05 12:30:00');
-      });
-    }, 2000);
+    const qrCode = await capturingQueryParam();
+    hideLoaderListener(() => {
+      document.querySelector('.js-main-content').innerHTML = HTMLMobileTemplate;
+      flipbookInitializer();
+      mobileListener(); // TO RESIZE FIPBOOK
+      counterListener('2022-02-05 12:30:00');
+      // INSERTING QR CODE IN THE DOM
+      renderQRCode(qrCode);
+    });
   }
 };
 setContentVariable(smallBp);
